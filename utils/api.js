259f77003cc2cf7c83937ac5f536d2f1db7b08b7
519430378qqcom <dgsimle@sina.com>
2017-10-18 15:33:16 +0800

    qisiwole

---
+++ utils/api.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  /**
+  * 热歌榜
+  */
+  hotVideoRank: "http://tingapi.ting.baidu.com/v1/restserver/ting?from=qianqian&version=2.1.0&method=baidu.ting.billboard.billList&format=json&type=2&offset=0&size=50",
+}
